1230048235c7d1d471451ca69f03f780ffbb9ad6
akiogitgit <akimaron2828@icloud.com>
2023-03-03 15:38:24 +0900

    feat: フォームバリデーション

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import { useCallback, useState } from 'react'
-import { Button, NumberInput } from '@mantine/core'
+import { Button, NumberInput, Stack } from '@mantine/core'
 import { Numbers, calcJamaica } from './calcJamaica'
-import { useForm } from '@mantine/form'
+import { useForm, zodResolver } from '@mantine/form'
+import { z } from 'zod'
 
 type FormValues = {
   answer: number
   numbers: Numbers
 }
+
+const scheme = z.object({
+  numbers: z
+    .number()
+    .min(1, { message: '白いサイコロの目は1以上で入力して下さい' })
+    .max(6, { message: '白いサイコロの目は6以下で入力して下さい' })
+    .array(),
+  answer: z
+    .number()
+    .min(11, { message: '黒いサイコロの目の和は11以上で入力して下さい' })
+    .max(66, { message: '黒いサイコロの目の和は66以下で入力して下さい' }),
+})
 
 function App() {
   const [results, setResults] = useState<string[]>()
@@ -16,39 +29,42 @@ function App() {
       answer: 11,
       numbers: [1, 2, 3, 4, 5],
     },
+    validate: zodResolver(scheme),
   })
 
-  const onSubmit = (values: FormValues) => {
+  const onSubmit = useCallback((values: FormValues) => {
     const { answer, numbers } = values
     const res = calcJamaica(numbers, answer)
     setResults(res)
-  }
-
-  // formいる？ いる。再レンダリング抑えられる => うそ
-  // フォームの所はコンポーネントに切り出そう
-  // 初期値入ってればいいんじゃね？
-  // NumberInputのnumは、1~6
-  // answerは、11,12,13,14,15,16,21,22,23,24,25,26
+  }, [])
 
   return (
     <div className='mx-auto max-w-150 w-80vw'>
       <h1 className='text-center'>Jamaica</h1>
 
       <form onSubmit={form.onSubmit(onSubmit)}>
-        {form.values.numbers.map((v, i) => (
+        <Stack>
+          {form.values.numbers.map((v, i) => (
+            <NumberInput
+              {...form.getInputProps(`numbers.${i}`)}
+              key={i}
+              min={1}
+              max={6}
+              label={'白い目'}
+            />
+          ))}
+
           <NumberInput
-            {...form.getInputProps(`numbers.${i}`)}
-            key={i}
-            min={1}
-            max={6}
+            {...form.getInputProps('answer')}
+            min={11}
+            max={66}
+            label={'黒い目の合計'}
           />
-        ))}
 
-        <NumberInput {...form.getInputProps('answer')} min={11} max={66} />
-
-        <Button type='submit' className='mt-10'>
-          実行
-        </Button>
+          <Button type='submit' className='mt-10'>
+            実行
+          </Button>
+        </Stack>
       </form>
 
       {JSON.stringify(results)}
