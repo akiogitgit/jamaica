c4c5d52c2fe210ffe36d071a4be2aa8fbdb94ecd
akiogitgit <akimaron2828@icloud.com>
2023-03-03 14:59:38 +0900

    feat: formのnumbersを配列で操作する

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,57 @@
-import { useState } from 'react'
-import { Button } from '@mantine/core'
-import { calcJamaica } from './calcJamaica'
+import { useCallback, useState } from 'react'
+import { Button, NumberInput } from '@mantine/core'
+import { Numbers, calcJamaica } from './calcJamaica'
+import { useForm } from '@mantine/form'
+
+type FormValues = {
+  answer: number
+  numbers: Numbers
+}
 
 function App() {
-  const [count, setCount] = useState(0)
   const [results, setResults] = useState<string[]>()
+
+  const form = useForm<FormValues>({
+    initialValues: {
+      answer: 11,
+      numbers: [1, 2, 3, 4, 5],
+    },
+  })
+
+  const onSubmit = (values: FormValues) => {
+    const { answer, numbers } = values
+    const res = calcJamaica(numbers, answer)
+    setResults(res)
+  }
+
+  // formいる？ いる。再レンダリング抑えられる => うそ
+  // フォームの所はコンポーネントに切り出そう
+  // 初期値入ってればいいんじゃね？
+  // NumberInputのnumは、1~6
+  // answerは、11,12,13,14,15,16,21,22,23,24,25,26
 
   return (
     <div className='mx-auto max-w-150 w-80vw'>
       <h1 className='text-center'>Jamaica</h1>
-      <Button
-        className='mt-10'
-        onClick={() => {
-          setResults(calcJamaica([1, 2, 3, 4, 5], 61))
-        }}
-      >
-        実行
-      </Button>
+
+      <form onSubmit={form.onSubmit(onSubmit)}>
+        {form.values.numbers.map((v, i) => (
+          <NumberInput
+            {...form.getInputProps(`numbers.${i}`)}
+            key={i}
+            min={1}
+            max={6}
+          />
+        ))}
+
+        <NumberInput {...form.getInputProps('answer')} min={11} max={66} />
+
+        <Button type='submit' className='mt-10'>
+          実行
+        </Button>
+      </form>
+
       {JSON.stringify(results)}
-      <div>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-      </div>
     </div>
   )
 }
