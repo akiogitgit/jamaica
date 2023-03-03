5adaec418208a9f80011052ef54629c4b9e5889a
akiogitgit <akimaron2828@icloud.com>
2023-03-03 11:17:02 +0900

    chore: コメント追加、関数の順番を変更

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,6 @@ import { calcJamaica } from './calcJamaica'
 function App() {
   const [count, setCount] = useState(0)
   const [results, setResults] = useState<string[]>()
-  // const res = calcJamaica([1, 2, 3, 4, 5], 61)
-  // console.log(res)
-
-  // 入力は　答えと、数字たち
-  // 出力は、答えになる式一覧
 
   return (
     <div className='mx-auto max-w-150 w-80vw'>
--- src/calcJamaica.ts
+++ src/calcJamaica.ts
@@ -6,26 +6,7 @@ const n = 4
 export const calcJamaica = (numbers: Numbers, answer: number) => {
   const results: string[] = []
 
-  // ++++, +++-, +++*, +++/, ++-+, ++*+, ++/+, ...
-  function operatorPermute(nums: Numbers, strOperators: string, len: number) {
-    // 記号が4つで計算する。 ex) res = 1+2+3+4+5
-    if (len === n) {
-      const calcFormula = `${nums[0]}${strOperators[0]}${nums[1]}${strOperators[1]}${nums[2]}${strOperators[2]}${nums[3]}${strOperators[3]}${nums[4]}`
-      const calcResult = looseJsonParse(calcFormula)
-
-      if (calcResult !== answer) return
-
-      results.push(`${calcFormula}=${calcResult}`)
-
-      return
-    }
-
-    // 1つずつ記号を取り出して、strSymbolsに追加する
-    for (let i = 0; i < n; i++) {
-      operatorPermute(nums, strOperators + operators[i], len + 1)
-    }
-  }
-
+  // 数字を総当たりで並び替え
   // 12345, 12354, 12435, 12453, ...
   for (let a of numbers) {
     for (let b of numbers) {
@@ -37,10 +18,36 @@ export const calcJamaica = (numbers: Numbers, answer: number) => {
           for (let e of numbers) {
             if (a === e || b === e || c === e || d === e) continue
 
-            operatorPermute([a, b, c, d, e], '', 0)
+            operatorPermute([a, b, c, d, e])
           }
         }
       }
+    }
+  }
+
+  // 記号を総当たりで並び替え
+  // ++++, +++-, +++*, +++/, ++-+, ++*+, ++/+, ...
+  function operatorPermute(
+    nums: Numbers,
+    strOperators: string = '',
+    len: number = 0,
+  ) {
+    // 記号が4つで計算する
+    // ex) res = 1 + 2 + 3 + 4 + 5
+    if (len === 4) {
+      const calcFormula = `${nums[0]}${strOperators[0]}${nums[1]}${strOperators[1]}${nums[2]}${strOperators[2]}${nums[3]}${strOperators[3]}${nums[4]}`
+      const calcResult = looseJsonParse(calcFormula)
+
+      if (calcResult !== answer) return
+
+      results.push(`${calcFormula}=${calcResult}`)
+
+      return
+    }
+
+    // 1つずつ記号を取り出して、strSymbolsに追加して再帰する
+    for (let i = 0; i < n; i++) {
+      operatorPermute(nums, strOperators + operators[i], len + 1)
     }
   }
 
